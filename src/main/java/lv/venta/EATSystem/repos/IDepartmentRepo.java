4985489301afc4ca7fa72e1981b9d73f1f318692
E. P. <eva.pankevica@gmail.com>
2025-11-08 19:41:06 +0200

    department service impl

--- src/main/java/lv/venta/EATSystem/repos/IDepartmentRepo.java
+++ src/main/java/lv/venta/EATSystem/repos/IDepartmentRepo.java
@@ -6,4 +6,8 @@ import lv.venta.EATSystem.models.Department;
 
 public interface IDepartmentRepo extends CrudRepository<Department, Integer>{
 
+	Department findByIdDepartment(int id);
+
+	void deleteByIdDepartment(int id);
+
 }
---
+++ src/main/java/lv/venta/EATSystem/services/impl/DepartmentServiceImpl.java
@@ -0,0 +1,46 @@
+package lv.venta.EATSystem.services.impl;
+
+import java.util.ArrayList;
+
+import lv.venta.EATSystem.models.Department;
+import lv.venta.EATSystem.models.Employee;
+import lv.venta.EATSystem.repos.IDepartmentRepo;
+import lv.venta.EATSystem.services.IDepartmentService;
+
+public class DepartmentServiceImpl implements IDepartmentService{
+	
+	private IDepartmentRepo departmentRepo;
+
+	@Override
+	public ArrayList<Department> selectAllDepartments() {
+		ArrayList<Department> result = (ArrayList<Department>) departmentRepo.findAll();
+		return result;
+	}
+
+	@Override
+	public Department selectDepartmentById(int id) throws Exception {
+		Department result = departmentRepo.findByIdDepartment(id);
+		return result;
+	}
+
+	@Override
+	public ArrayList<Department> deleteDepartmentById(int id) {
+		departmentRepo.deleteByIdDepartment(id);
+		ArrayList<Department> result = selectAllDepartments();
+		return result;
+	}
+
+	@Override
+	public Department insertNewDepartment(String title, Employee manager) {
+		Department result = new Department(title, manager);
+		return result;
+	}
+
+	@Override
+	public Department updateDepartmentById(int id, String title, Employee manager) throws Exception {
+		Department result = departmentRepo.findByIdDepartment(id);
+		result = new Department (title, manager);
+		return result;
+	}
+
+}
