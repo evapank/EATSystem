d5d345169826a2e6422539e4df457b13ce18cb63
E. P. <eva.pankevica@gmail.com>
2025-11-08 15:47:43 +0200

    employee service

---
+++ src/main/java/lv/venta/EATSystem/services/IEmployeeService.java
@@ -0,0 +1,20 @@
+package lv.venta.EATSystem.services;
+
+import java.util.ArrayList;
+
+import lv.venta.EATSystem.models.Department;
+import lv.venta.EATSystem.models.Employee;
+
+public interface IEmployeeService {
+	
+public abstract ArrayList<Employee> selectAllEmployees();
+	
+	public abstract Employee selectEmployeeById(int id) throws Exception;
+	
+	public abstract ArrayList<Employee> deleteEmployeeById(int id);
+	
+	public abstract Employee insertNewEmployee(String name, String surname, String position, Department department, String email);
+	
+	public abstract Employee updateEmployeeById(int id, String name, String surname, String position, Department department, String email) throws Exception;
+
+}
