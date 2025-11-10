13332ad45b59bea63021c6c721504b598a616bd8
E. P. <eva.pankevica@gmail.com>
2025-11-10 23:35:51 +0200

    fix

--- src/main/java/lv/venta/EATSystem/controllers/DepartmentController.java
+++ src/main/java/lv/venta/EATSystem/controllers/DepartmentController.java
@@ -5,23 +5,26 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.validation.Valid;
 import lv.venta.EATSystem.models.Department;
 import lv.venta.EATSystem.services.IDepartmentService;
 
+@RequestMapping("/department")
 public class DepartmentController {
 	
 	@Autowired
 	IDepartmentService departmentService;
 	
-	@GetMapping("/department/all")
+	@GetMapping("/all")
 	public String getAllDepartments(Model model) {
 		model.addAttribute("department", departmentService.selectAllDepartments());
 		return "department-all-page";
 	}
 	
-	@GetMapping("/department/all/{id}")
+	@GetMapping("/all/{id}")
 	public String getDepartmentById(Model model, @PathVariable(name = "id") int id) {
 		try {
 			model.addAttribute("department", departmentService.selectDepartmentById(id));
@@ -32,17 +35,18 @@ public class DepartmentController {
 		return "department-one-page";
 	}
 
-	@GetMapping("/department/remove/{id}")
+	@GetMapping("/remove/{id}")
 	public String deleteDepartmentById(Model model, @PathVariable(name = "id") int id) {
 		model.addAttribute("department", departmentService.deleteDepartmentById(id));
 		return "department-all-page";
 	}
 	
-	@GetMapping("/department/create")
+	@GetMapping("/create")
 	public String getAddDepartment(Department department) {
 		return "department-add-page";
 	}
 	
+	@PostMapping("/create")
 	public String postAddDepartment (@Valid Department department, BindingResult result) {
 		if(!result.hasErrors()) {
 			departmentService.insertNewDepartment(department.getTitle(), department.getManager());
