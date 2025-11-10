6ecf880b385c6a954062c664299340065c48d53d
E. P. <eva.pankevica@gmail.com>
2025-11-10 23:32:18 +0200

    department controller

--- src/main/java/lv/venta/EATSystem/controllers/DepartmentController.java
+++ src/main/java/lv/venta/EATSystem/controllers/DepartmentController.java
@@ -1,6 +1,53 @@
 package lv.venta.EATSystem.controllers;
 
-public class DepartmentController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import jakarta.validation.Valid;
+import lv.venta.EATSystem.models.Department;
+import lv.venta.EATSystem.services.IDepartmentService;
+
+public class DepartmentController {
+	
+	@Autowired
+	IDepartmentService departmentService;
+	
+	@GetMapping("/department/all")
+	public String getAllDepartments(Model model) {
+		model.addAttribute("department", departmentService.selectAllDepartments());
+		return "department-all-page";
+	}
+	
+	@GetMapping("/department/all/{id}")
+	public String getDepartmentById(Model model, @PathVariable(name = "id") int id) {
+		try {
+			model.addAttribute("department", departmentService.selectDepartmentById(id));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "department-one-page";
+	}
+
+	@GetMapping("/department/remove/{id}")
+	public String deleteDepartmentById(Model model, @PathVariable(name = "id") int id) {
+		model.addAttribute("department", departmentService.deleteDepartmentById(id));
+		return "department-all-page";
+	}
+	
+	@GetMapping("/department/create")
+	public String getAddDepartment(Department department) {
+		return "department-add-page";
+	}
+	
+	public String postAddDepartment (@Valid Department department, BindingResult result) {
+		if(!result.hasErrors()) {
+			departmentService.insertNewDepartment(department.getTitle(), department.getManager());
+		}
+		return "department-add-page";
+	}
 
 }
