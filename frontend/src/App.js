614fab88e3097550a19637825e4bd0e7169930a7
E. P. <eva.pankevica@gmail.com>
2025-12-12 19:37:22 +0200

    routes added in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AllDepartments from './department/AllDepartments';
+import OneDepartment from './department/OneDepartment';
+import CreateDepartment from './department/CreateDepartment';
+import UpdateDepartment from './department/UpdateDepartment';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path='/department/all' exact={true} element={<AllDepartments/>}/>
+        <Route path={`/department/all/${id}`} exact={true} element={<OneDepartment/>}/>
+        <Route path={`/department/create`} exact={true} element={<CreateDepartment/>}/>
+        <Route path={`/department/update/${id}`} exact={true} element={<UpdateDepartment/>}/>
       </Routes>
     </Router>
   )
