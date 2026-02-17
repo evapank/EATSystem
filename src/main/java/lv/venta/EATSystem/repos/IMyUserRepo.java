60dd9f78f340b7e68bbf1ae2be892497edbaa47e
E. P. <eva.pankevica@gmail.com>
2026-02-17 18:35:05 +0200

    imyuserrepo added

---
+++ src/main/java/lv/venta/EATSystem/repos/IMyUserRepo.java
@@ -0,0 +1,10 @@
+package lv.venta.EATSystem.repos;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import lv.venta.EATSystem.models.MyUser;
+
+@Repository
+public interface IMyUserRepo extends MongoRepository<MyUser,String>{
+
+}
