92d640a7fd50a2539e35e338e268ad46d029d235
E. P. <eva.pankevica@gmail.com>
2026-02-18 21:22:24 +0200

    imyauthorityrepo created

---
+++ src/main/java/lv/venta/EATSystem/repos/IMyAuthorityRepo.java
@@ -0,0 +1,9 @@
+package lv.venta.EATSystem.repos;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.venta.EATSystem.models.MyAuthority;
+
+public interface IMyAuthorityRepo extends CrudRepository<MyAuthority, Integer>{
+
+}
