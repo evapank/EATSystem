8cbd1de6af9da6d827f8b6fe95606a170230ba9c
E. P. <eva.pankevica@gmail.com>
2026-03-30 23:00:37 +0300

    onChange function in newMeeting.js

--- frontend/src/meeting/NewMeeting.js
+++ frontend/src/meeting/NewMeeting.js
@@ -1,6 +1,12 @@
-const NewMeeting = () => {
-const [employeeStatuses, setEmployeeStatuses] = useState([]);
-const [dateTime, setDateTime] = useState({});
-};
+import { MeetingService } from "../static/api";
 
+const NewMeeting = () => {
+    const [employeeStatuses, setEmployeeStatuses] = useState([]);
+    const [dateTime, setDateTime] = useState({});
+
+    const onChangeDateTime = async (dateTime) => {
+        const response = await MeetingService.getEmployeeStatuses(dateTime);
+        setDateTime(response.data);
+    };
+}
 export default NewMeeting;
