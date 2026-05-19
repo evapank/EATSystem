aff7b9e6384218f53675dbe1c27a89db69199f51
E. P. <eva.pankevica@gmail.com>
2026-05-19 16:40:09 +0300

    eosservicetest fixed

--- src/test/java/lv/venta/EATSystem/services/EmployeeOrderStatusServiceTest.java
+++ src/test/java/lv/venta/EATSystem/services/EmployeeOrderStatusServiceTest.java
@@ -1,29 +1,41 @@
 package lv.venta.EATSystem.services;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import lv.venta.EATSystem.enums.GeneralStatus;
+import lv.venta.EATSystem.models.Employee;
 import lv.venta.EATSystem.models.EmployeeOrderStatus;
 import lv.venta.EATSystem.repos.IEmployeeOrderStatusRepo;
 
 @SpringBootTest(properties = "spring.profiles.active=test")
+@AutoConfigureMockMvc
 class EmployeeOrderStatusServiceTest {
 
-	@Autowired
+	@InjectMocks
 	IEmployeeOrderStatusService eosService;
 	
-	@Autowired
+	@Mock
 	IEmployeeOrderStatusRepo eosRepo;
 	
 	@Test
-	void testSelectAll() {
-		EmployeeOrderStatus eos = new EmployeeOrderStatus();
-		eosRepo.save(eos);
+	void testCreate() {
+		Employee employee = new Employee();
+		EmployeeOrderStatus eos = new EmployeeOrderStatus(employee, GeneralStatus.DayOff);
 		
-		assertFalse(eosService.selectAllEmployeeOrderStatuses().isEmpty());
+		when(eosRepo.save(any())).thenReturn(eos);
+		
+		EmployeeOrderStatus eosFromService = eosService.insertNewEmployeeOrderStatus(employee, eos.getGeneralStatus());
+		
+		assertEquals(GeneralStatus.DayOff, eosFromService.getGeneralStatus());
 	}
 
 }
