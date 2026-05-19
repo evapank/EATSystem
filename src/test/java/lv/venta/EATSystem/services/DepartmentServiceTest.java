cc31483d746f6e1c43416ab8d47c777013bbccdf
E. P. <eva.pankevica@gmail.com>
2026-05-19 17:34:18 +0300

    DepartmentServiceTest NPE fixed

--- src/test/java/lv/venta/EATSystem/services/DepartmentServiceTest.java
+++ src/test/java/lv/venta/EATSystem/services/DepartmentServiceTest.java
@@ -6,11 +6,13 @@ import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -18,16 +20,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 import lv.venta.EATSystem.models.Department;
 import lv.venta.EATSystem.models.Employee;
 import lv.venta.EATSystem.repos.IDepartmentRepo;
+import lv.venta.EATSystem.repos.IEmployeeRepo;
+import lv.venta.EATSystem.services.impl.DepartmentServiceImpl;
 
 @SpringBootTest(properties = "spring.profiles.active=test")
-@AutoConfigureMockMvc
 class DepartmentServiceTest {
-
-	@InjectMocks
-	IDepartmentService departmentService;
 	
 	@Mock
-	IDepartmentRepo departmentRepo;
+	private static IDepartmentRepo departmentRepo;
+	
+	@InjectMocks
+	private static DepartmentServiceImpl departmentService;
+	
+	@Mock
+	private IEmployeeRepo employeeRepo;
+	
+	@BeforeEach
+	void setUp()
+	{
+		MockitoAnnotations.openMocks(this);
+	}
 	
 	@Test
 	void testCreate() {
@@ -35,7 +47,10 @@ class DepartmentServiceTest {
 		Employee employee = new Employee();
 		
 		when(departmentRepo.save(any())).thenReturn(department);
+		when(employeeRepo.findByIdEmployee(employee.getIdEmployee())).thenReturn(employee);
+		when(employeeRepo.save(any())).thenReturn(employee);
 		
+		employeeRepo.save(employee);
 		Department departmentFromService = departmentService.insertNewDepartment(department.getTitle(), employee.getIdEmployee());
 		
 		assertEquals("Department", departmentFromService.getTitle());
--- src/test/java/lv/venta/EATSystem/services/EmployeeServiceTest.java
+++ src/test/java/lv/venta/EATSystem/services/EmployeeServiceTest.java
@@ -1,29 +1,49 @@
 package lv.venta.EATSystem.services;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import lv.venta.EATSystem.models.Department;
 import lv.venta.EATSystem.models.Employee;
 import lv.venta.EATSystem.repos.IEmployeeRepo;
 
 @SpringBootTest(properties = "spring.profiles.active=test")
+@AutoConfigureMockMvc
 class EmployeeServiceTest {
 
-	@Autowired
+	@InjectMocks
 	IEmployeeService employeeService;
 	
-	@Autowired
+	@Mock
 	IEmployeeRepo employeeRepo;
 	
+	@BeforeEach
+	void setUp()
+	{
+		MockitoAnnotations.openMocks(this);
+	}
+	
 	@Test
-	void testSelectAll() {
-		Employee employee = new Employee();
-		employeeRepo.save(employee);
+	void testCreate() {
+		Department department = new Department();
+		Employee employee = new Employee("Laura", "Berzina", "Social media manager", department, "lberzina1990@gmail.com", true);
 		
-		assertFalse(employeeService.selectAllEmployees().isEmpty());
+		when(employeeRepo.save(any())).thenReturn(employee);
+		
+		Employee employeeFromService = employeeService.insertNewEmployee(employee.getName(), employee.getSurname(), employee.getPosition(),
+				employee.getDepartment(), employee.getEmail(), employee.isManager());
+		
+		assertEquals("Berzina", employeeFromService.getSurname());
 	}
 
 }
