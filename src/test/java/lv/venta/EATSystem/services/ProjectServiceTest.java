f2e08e5f47be605632121f28b8265f1686f371c8
E. P. <eva.pankevica@gmail.com>
2026-05-19 19:34:16 +0300

    projectServiceTest fixed

--- src/test/java/lv/venta/EATSystem/services/ProjectServiceTest.java
+++ src/test/java/lv/venta/EATSystem/services/ProjectServiceTest.java
@@ -1,29 +1,44 @@
 package lv.venta.EATSystem.services;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.time.Month;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import lv.venta.EATSystem.models.Employee;
 import lv.venta.EATSystem.models.Project;
 import lv.venta.EATSystem.repos.IProjectRepo;
+import lv.venta.EATSystem.services.impl.ProjectServiceImpl;
 
 @SpringBootTest(properties = "spring.profiles.active=test")
 class ProjectServiceTest {
 
-	@Autowired
-	IProjectService projectService;
+	@InjectMocks
+	ProjectServiceImpl projectService;
 	
-	@Autowired
+	@Mock
 	IProjectRepo projectRepo;
 	
 	@Test
-	void testSelectAll() {
-		Project project = new Project();
-		projectRepo.save(project);
+	void testCreate() {
+		Employee employee = new Employee();
+		Project project = new Project(54321, "Full-stack development", LocalDate.of(2026, Month.APRIL, 19),
+				LocalDate.of(2027, Month.DECEMBER, 29), employee);
 		
-		assertFalse(projectService.selectAllProjects().isEmpty());
+		when(projectRepo.save(any())).thenReturn(project);
+		
+		Project projectFromService = projectService.insertNewProject(project.getProjectNumber(), project.getTitle(),
+				project.getDateStart(), project.getDateEnd(), project.getProjectManager());
+		
+		assertEquals("Full-stack development", projectFromService.getTitle());
 	}
 
 }
