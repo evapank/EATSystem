d5f5987b3a41a4f484bcffd7dbd083c3766fb4b9
E. P. <eva.pankevica@gmail.com>
2026-05-19 17:42:13 +0300

    employeeStatusServiceTest fixed

--- src/test/java/lv/venta/EATSystem/services/EmployeeStatusServiceTest.java
+++ src/test/java/lv/venta/EATSystem/services/EmployeeStatusServiceTest.java
@@ -1,29 +1,50 @@
 package lv.venta.EATSystem.services;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import lv.venta.EATSystem.enums.GeneralStatus;
+import lv.venta.EATSystem.models.Employee;
 import lv.venta.EATSystem.models.EmployeeStatus;
 import lv.venta.EATSystem.repos.IEmployeeStatusRepo;
+import lv.venta.EATSystem.services.impl.EmployeeStatusServiceImpl;
 
 @SpringBootTest(properties = "spring.profiles.active=test")
 class EmployeeStatusServiceTest {
 
-	@Autowired
-	IEmployeeStatusService emplStService;
+	@InjectMocks
+	EmployeeStatusServiceImpl emplStService;
 	
-	@Autowired
+	@Mock
 	IEmployeeStatusRepo empStRepo;
 	
+	@BeforeEach
+	void setUp()
+	{
+		MockitoAnnotations.openMocks(this);
+	}
+	
 	@Test
-	void testSelectAll() {
-		EmployeeStatus empSt = new EmployeeStatus();
-		empStRepo.save(empSt);
+	void testCreate() {
+		Employee employee = new Employee();
+		EmployeeStatus empSt = new EmployeeStatus(employee, GeneralStatus.InPerson,
+				LocalDateTime.of(2026, Month.JULY, 7, 12, 30), LocalDateTime.of(2026, Month.JULY, 7, 16, 30));
 		
-		assertFalse(emplStService.selectAllEmployeeStatuses().isEmpty());
+		when(empStRepo.save(any())).thenReturn(empSt);
+		
+		assertEquals(LocalDateTime.of(2026, Month.JULY, 7, 12, 30), empSt.getDateTimeStart());
 	}
 
 }
