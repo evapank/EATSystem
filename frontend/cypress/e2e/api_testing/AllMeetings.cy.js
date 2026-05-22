0790fad30a72b5b3da598400b3a083ad2682f641
E. P. <eva.pankevica@gmail.com>
2026-05-22 19:45:38 +0300

    AllOrders cypress test added

---
+++ frontend/cypress/e2e/api_testing/AllMeetings.cy.js
@@ -0,0 +1,14 @@
+const MockMeetings = [
+   
+]
+
+describe("Get all meetings", ()=> {
+   // it("Intercept request", ()=>{
+    //    cy.visit('http://localhost:3000/meeting/all')
+    //    cy.intercept("GET", "/meeeting/all", MockMeetings).as("AllMeetings");
+        
+    //    cy.request("/meeeting/all")
+    //    cy.get("calendar").should("contain", "Online")
+   // });
+
+});
