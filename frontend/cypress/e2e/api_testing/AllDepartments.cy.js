9a385b6598dc93e58555f538374bac85ef9c80a9
E. P. <eva.pankevica@gmail.com>
2026-05-22 17:38:03 +0300

    AllDepartments cypress test added

---
+++ frontend/cypress/e2e/api_testing/AllDepartments.cy.js
@@ -0,0 +1,23 @@
+import AllDepartments from "../../../src/department/AllDepartments";
+
+const MockDepartments = [
+    {
+        "idDepartment": 1,
+        "title": "UI design"
+    },
+    {
+        "idDepartment": 2,
+        "title": "Frontend testing"
+    }
+]
+
+describe("Get all departments", ()=> {
+    it("Intercept request", ()=>{
+        cy.visit('http://localhost:3000/department/all')
+        cy.intercept("GET", "/department/all", MockDepartments).as("AllDepartments");
+        
+        cy.request("/department/all")
+        cy.get("table").should("contain", "UI design").and("contain", "Frontend testing")
+    });
+
+});
